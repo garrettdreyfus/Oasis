46d48b6e97d7d0d8f3ca7777ead26d0100114503
garrettdreyfus <garrettdreyfus@gmail.com>
2014-03-19 08:55:15 -0400

    bunny model finished not tested

--- AnimalModels/bunny.js
+++ AnimalModels/bunny.js
@@ -1,15 +1,3 @@
-//Bunny
-// x,y
-// health , hunger, thirst, age
-// speed , sightDist
-function cap (num)
-{
-	if (num>100)
-	{
-		num=100;
-	}
-	return num;
-}
 function Bunny (x,y,health,hunger,thirst,age,speed,sightDist)
 {
 	this.x = x;
@@ -20,6 +8,7 @@ function Bunny (x,y,health,hunger,thirst,age,speed,sightDist)
 	this.age = age;
 	this.speed = speed;
 	this.sightDist  = sightDist;
+	this.type='bunny';
 	//get attribute
 	this.get = function (attr)
 	{
@@ -45,18 +34,65 @@ function Bunny (x,y,health,hunger,thirst,age,speed,sightDist)
 		}
 	}
 	//returns Water Heuristic 
-	this.waterHeur = function (world)
+	this.waterHeur = function (x,y,world)
 	{
-		return world.closestWaterDistance(this.get('x'),this.get('y')) * this.get('thirst')
+		return world.closestWaterDistance(x,y) * this.get('thirst');
 	}
 	//returns Grass Heuristic 
-	this.grassHeur = function (world)
+	this.grassHeur = function (x,y,world)
 	{
-		return world.closestGrassDistance(this.get('x'),this.get('y')) * this.get('hunger')
+		return world.closestGrassDistance(x,y)* this.get('hunger');
+	}
+	this.wolfHeur = function (x,y,world)
+	{
+		var heursum = 0;
+		var inrange = world.scanInRange(this.get('x'),this.get('y'),this.get('sightDist'));
+		for(var i=0; i< inrange.length; i++)
+		{
+			if(inrange[i].type=='wolf')
+			{
+				heursum += Math.abs(x-inrange[i].get('x')) + Math.abs(y-inrange[i].get('y'));
+			}
+		}
+		if(heursum != 0)
+		{
+			return (1/heursum)*-50000;
+		}
+		else{
+			return 0	
+		}
+	}
+	//total heuristic
+	this.totalHeuristic = function (x,y,world)
+	{
+		var totalH;
+		totalH += this.waterHeur(x,y,world);
+		totalH += this.grassHeur(x,y,world);
+		totalH += this.wolfHeur(x,y,world);
+		return totalH;
 	}
 	//makes the decison
 	this.decide = function (world)
 	{
+		var xArr = [-1,0,1];
+		var yArr = [-1,0,1];
+		var maximumHeur;
+		for (var xc=0; xc<xArr.length; xc++)
+		{
+			var x = xArr[xc];
+			for (var yc=0; yc<yArr.length; yc++)
+			{
+				var y = yArr[yc];
+				var totalHeur = this.totalHeuristic(x,y,world);
+				if(totalHeur>maximumHeur[0])
+				{
+					maximumHeur = [totalHeur,x,y];
+				}
+			}
+		}
+		this.set('x',maximumHeur[1])
+		this.set('y',maximumHeur[2])
+		
 	}
 	//ages the animal by one, decreades hunger thirst and age + decides
 	this.timestep = function(world)
